37bf83bd54f44dfdf820da1a20ddf78a24dabc0a
MUHAMMAD FIQHI FAUZAN <mhmmdfiqhi93@gmail.com>
2024-06-19 20:25:31 +0800

    add calculate & logo

--- JS/script.js
+++ JS/script.js
@@ -7,8 +7,10 @@ document
     );
     const unit = document.getElementById("unit").value;
     let result;
+    let calculation;
     if (unit === "celsius") {
       result = (temperature * 9) / 5 + 32;
+      calculation = `${temperature}°C x 9/5 + 32 = ${result.toFixed(2)}°F`;
       document.getElementById(
         "result"
       ).textContent = `Hasil dari ${temperature}°C setelah di konversi adalah ${result.toFixed(
@@ -16,16 +18,21 @@ document
       )}°F`;
     } else if (unit === "fahrenheit") {
       result = ((temperature - 32) * 5) / 9;
+      calculation = `(${temperature} - 32)°F x 5/9 = ${result.toFixed(2)}°C`;
       document.getElementById(
         "result"
       ).textContent = `Hasil dari ${temperature}°F setelah di konversi adalah ${result.toFixed(
         2
       )}°C`;
     }
+    document.getElementById(
+      "calculation-method"
+    ).textContent = `Metode Perhitungan: ${calculation}`;
   });
 
 document
   .getElementById("converter-form")
   .addEventListener("reset", function () {
     document.getElementById("result").textContent = "";
+    document.getElementById("calculation-method").textContent = "";
   });
